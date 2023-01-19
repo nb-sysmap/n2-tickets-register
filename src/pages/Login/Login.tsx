2b7ecf29b829937ee890847ba592680a013e3c90
Vitor <costec222@gmail.com>
2023-01-19 17:25:43 -0300

    feat: authentication with microsoft added

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { OAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
+import { app } from '../../services/auth-firebase-provider/firebaseConfig'
+
+const provider = new OAuthProvider('microsoft.com')
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+  const auth = getAuth(app)
+  function signInMicrosoft() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = OAuthProvider.credentialFromResult(result)!
+        const accessToken = credential.accessToken
+        const idToken = credential.idToken
+        const user = result.user
+        console.log(user)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  return <button onClick={signInMicrosoft}>Logar com Microsoft</button>
 }
