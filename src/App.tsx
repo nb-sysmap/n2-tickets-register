d2a18b148d4b6c3cbbf3771c082859e09a804a21
Vitor <costec222@gmail.com>
2023-01-19 15:54:46 -0300

    add pages login and home with react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { AppRoutes } from './routes/routes'
 
 export function App() {
 
   return (
     <ChakraProvider>
-      <h1>iniciando</h1>
+      <AppRoutes />
     </ChakraProvider>
   )
 }
