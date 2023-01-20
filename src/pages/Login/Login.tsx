6168e3890fa459c7ad7fb4d008b10dc019a85216
Vitor <costec222@gmail.com>
2023-01-19 21:34:21 -0300

    feat: finish setup oauth microsoft provider with firebase

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,22 +1,37 @@
 import { OAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
-import { app } from '../../services/auth-firebase-provider/firebaseConfig'
+import { app } from '../../services/firebase/firebaseConfig'
+import { getMicrosoftProfile } from '../../services/microsoftProfile/microsoftProfile';
 
 const provider = new OAuthProvider('microsoft.com')
 
 export default function Login() {
+  let profileImg = '';
+  
   const auth = getAuth(app)
   function signInMicrosoft() {
     signInWithPopup(auth, provider)
       .then((result) => {
         const credential = OAuthProvider.credentialFromResult(result)!
-        const accessToken = credential.accessToken
-        const idToken = credential.idToken
+        const accessToken = credential.accessToken //credential.accessToken
         const user = result.user
-        console.log(user)
+        const idToken = user.providerData[0].uid
+        console.log(idToken)
+
+        getMicrosoftProfile(accessToken!, idToken!)
+        
       })
       .catch((error) => {
         console.log(error)
       })
   }
-  return <button onClick={signInMicrosoft}>Logar com Microsoft</button>
+  return (
+    <>
+      <button onClick={signInMicrosoft}>Logar com Microsoft</button>
+      <div>
+        <img src={profileImg} alt="" />
+      </div>
+    </>
+  )
+  
+  
 }
